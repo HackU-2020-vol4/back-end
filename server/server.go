d150d73f47f86974b3aeb415192cc1cc7df8bc0a
fuki <fuki01@users.noreply.github.com>
2021-03-02 21:51:15 +0900

    delete: 必要がなくなったファイルの削除

--- server/server.go
+++ server/server.go
@@ -2,8 +2,6 @@ package server
 
 import (
 	"back-end/controller/publisher"
-	"back-end/controller/room"
-	"back-end/controller/user"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,24 +14,6 @@ func Init() {
 //routerの詳細 https://pkg.go.dev/github.com/gin-gonic/gin#readme-grouping-routes
 func router() *gin.Engine {
 	router := gin.Default()
-	users := router.Group("/users")
-	{
-		user := user.UserController{}
-		users.GET("", user.Index)
-		users.GET("/:id", user.Show)
-		users.POST("", user.Create)
-		users.PUT("/:id", user.Update)
-		users.DELETE("/:id", user.Delete)
-	}
-	rooms := router.Group("/rooms")
-	{
-		room := room.RoomController{}
-		rooms.GET("", room.Index)
-		rooms.GET("/:id", room.Show)
-		rooms.POST("", room.Create)
-		rooms.PUT("/:id", room.Update)
-		rooms.DELETE("/:id", room.Delete)
-	}
 	publishers := router.Group("/publishers")
 	{
 		publisher := publisher.PublisherController{}
