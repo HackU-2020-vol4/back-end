155835a82d6ce949baad05bb9d3ac23863919af8
fuki <fuki01@users.noreply.github.com>
2021-03-04 19:47:49 +0900

    add: keywordの実装

---
+++ controller/keyword/keyword_controller.go
@@ -0,0 +1,61 @@
+package keyword
+
+import (
+	"github.com/HackU-2020-vol4/back-end/service/keyword"
+
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type KeywordController struct{}
+
+//index
+func (pc KeywordController) Index(c *gin.Context) {
+	publisherID := c.Params.ByName("publisherID")
+	var s keyword.Service
+	p, err := s.GetByPublisher(publisherID)
+	if err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, p)
+	}
+}
+
+//create
+func (pc KeywordController) Create(c *gin.Context) {
+	var s keyword.Service
+	p, err := s.CreateModel(c)
+	if err != nil {
+		c.AbortWithStatus(400)
+		fmt.Println(err)
+	} else {
+		c.JSON(201, p)
+	}
+}
+
+//Destroy
+func (pc KeywordController) Destroy(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var s keyword.Service
+	if err := s.DeleteByID(id); err != nil {
+		c.AbortWithStatus(403)
+		fmt.Println(err)
+	} else {
+		c.JSON(204, gin.H{"id #" + id: "deleted"})
+	}
+}
+
+//update
+func (pc KeywordController) Update(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var s keyword.Service
+	p, err := s.UpdateByID(id, c)
+	if err != nil {
+		c.AbortWithStatus(400)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, p)
+	}
+}
--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"back-end/controller/publisher"
 
+	"github.com/HackU-2020-vol4/back-end/controller/keyword"
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,10 +15,20 @@ func Init() {
 //routerの詳細 https://pkg.go.dev/github.com/gin-gonic/gin#readme-grouping-routes
 func router() *gin.Engine {
 	router := gin.Default()
+
 	publishers := router.Group("/publishers")
 	{
 		publisher := publisher.PublisherController{}
 		publishers.POST("/", publisher.Create)
 	}
+	keywords := router.Group("/keywords")
+	{
+		keyword := keyword.KeywordController{}
+		keywords.GET("/:publisherID", keyword.Index)
+		keywords.POST("/:publisherID/create", keyword.Create)
+		keywords.DELETE("/:id", keyword.Destroy)
+		keywords.PUT("/:id", keyword.Update)
+	}
+
 	return router
 }
